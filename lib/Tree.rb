6d0092b2de904761b0c66b303bb0461b89ad0b35
Kristian Perez <kpere192@fiu.edu>
2024-04-22 13:13:02 -0400

    Implemented pretty print of balanced tree

--- lib/Tree.rb
+++ lib/Tree.rb
@@ -25,8 +25,9 @@ class Tree
 
   def pretty_print(node = @root, prefix = '', is_left = true)
   pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
-  puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+  puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
   pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
+
 
 end
--- lib/main.rb
+++ lib/main.rb
@@ -2,4 +2,6 @@ require_relative "Tree"
 require_relative "Node"
 
 test_tree = Tree.new([4,3,2,1,1,5,7,8])
-p test_tree.build_tree
+test_tree.root = test_tree.build_tree
+
+test_tree.pretty_print
