6a92b454c4153c31a3020bfb7be7f230aae8a5d6
Kristian Perez <kpere192@fiu.edu>
2024-04-22 13:16:53 -0400

    Turned build_tree into the recursive method instead of have 2 seperate ones. Now uses default values so it can be called with no params

--- lib/Tree.rb
+++ lib/Tree.rb
@@ -2,31 +2,28 @@ class Tree
   attr_accessor :root, :nodes
 
   def initialize(nodes)
-    @nodes = nodes
+    @nodes = nodes.uniq.sort
   end
 
-  def build_tree()
-    sort_nodes = nodes.uniq.sort
-    recursive_build_tree(sort_nodes, 0, sort_nodes.length - 1)
-  end
-
-  def recursive_build_tree(nodes, start, finish)
+  def build_tree(nodes = @nodes, start = 0, finish = @nodes.length - 1)
     if(start > finish)
       return
     end
     mid = (start + finish)/ 2
     root = Node.new(nodes[mid])
 
-    root.left = recursive_build_tree(nodes, start, mid-1)
-    root.right = recursive_build_tree(nodes, mid+1, finish)
+    root.left = build_tree(nodes, start, mid-1)
+    root.right = build_tree(nodes, mid+1, finish)
 
     root
   end
 
+
+
   def pretty_print(node = @root, prefix = '', is_left = true)
-  pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
-  puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
-  pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 
 
